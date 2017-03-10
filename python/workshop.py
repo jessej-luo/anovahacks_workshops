ef833d609350866d38e1b9982d52bbfedfed1d2d
Jesse Luo <jessej.luo@berkeley.edu>
2017-03-09 23:36:25 -0800

    ok

--- python/workshop.py
+++ python/workshop.py
@@ -3,25 +3,27 @@ a = 5
 b = 6
 
 #make c equal to 11 using a and b
-c = 
+c = 0
 
 #make c equal to 1 using a and b
-c = 
+c = 0
 
 #create the sentence "Hello World" using the + sign
-sentence = 
+sentence = 0
 
 
 #if c > 5, print "hello", else print "goodbye"
-if ():
-
-else: 
+if (0):
+	return None
+else:
+	return None 
 
 #define function to add a and b
 def adder(a, b): 
+	return None
 
 #create a four item list of your favorite colors
-favorite_colors = 
+favorite_colors = 0
 
 
 #access your first and third favorite color!
