652ec9b6bd20cdae60496e5398eac01af24418b7
Jesse Luo <jessej.luo@berkeley.edu>
2017-03-09 23:20:03 -0800

    added setup

--- flask/app.py
+++ flask/app.py
@@ -6,4 +6,4 @@ def hello():
 	return "Hello World!" 
 
 if __name__ == "__main__": 
-	app.run()
+	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Beginning setup of your Cloud 9 Workspace"
+
+echo ""
+echo "Installing Flask"
+sudo easy_install flask
+
+echo ""
+echo "Setting permissions"
+chmod a-wx Resources.txt#!/bin/bash
