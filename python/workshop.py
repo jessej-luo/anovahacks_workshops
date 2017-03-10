f340506eb1f64c47527bcd21e7ec17a38f1d2c78
Jesse Luo <jessej.luo@berkeley.edu>
2017-03-09 23:05:36 -0800

    git workshop

---
+++ python/workshop.py
@@ -0,0 +1,27 @@
+#Assign a value to a variable
+a = 5
+b = 6
+
+#make c equal to 11 using a and b
+c = 
+
+#make c equal to 1 using a and b
+c = 
+
+#create the sentence "Hello World" using the + sign
+sentence = 
+
+
+#if c > 5, print "hello", else print "goodbye"
+if ():
+
+else: 
+
+#define function to add a and b
+def adder(a, b): 
+
+#create a four item list of your favorite colors
+favorite_colors = 
+
+
+#access your first and third favorite color!
