a30fcb6fb8c1e866bd13ec2dc261ec613081c7cd
Jesse Luo <jessej.luo@berkeley.edu>
2017-03-09 23:26:39 -0800

    pls owrk

--- setup.sh
+++ setup.sh
@@ -2,10 +2,27 @@
 
 echo "Beginning setup of your Cloud 9 Workspace"
 
+
 echo ""
-echo "Installing Flask"
-sudo easy_install flask
+echo "Installing pip, Python3's package manager"
+sudo easy_install3 pip
+
+echo ""
+echo "Installing flask"
+sudo python3 -m pip install flask
+
+echo ""
+echo "Installing NPM (C9.IDE.Desktop dependency)"
+sudo pip3 install npm
+
+echo ""
+echo "Installing websockify"
+sudo apt-get install websockify
+
+echo ""
+echo "Installing C9 Desktop"
+c9 install c9.ide.desktop --force
 
 echo ""
 echo "Setting permissions"
-chmod a-wx Resources.txt#!/bin/bash
+chmod a-wx Resources.txt
